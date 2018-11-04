cf2ec77c6b7740807294e5d98b584081434bb652
Allen Tuggle <allen.tuggle@excella.com>
2018-11-04 17:04:09 -0500

    Initial commit with C++ Hellow World

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    
+    std::cout << "########" << std::endl;
+    std::cout << "Hello World: " << c << std::endl;
+    std::cout << "########" << std::endl;
+
+    return 0;
+}
